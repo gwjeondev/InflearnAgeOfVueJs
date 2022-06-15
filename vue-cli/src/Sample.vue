b44b1227704343ea8d25c654f8bbb8c7980fdf3f
gwjeondev <gw4609@gmail.com>
2022-06-15 10:27:19 +0900

    프로젝트 생성 도구 - Vue CLI - 싱글파일 컴포넌트 소개 및 여태까지 배운 내용 적용하는 방법

---
+++ vue-cli/src/Sample.vue
@@ -0,0 +1,26 @@
+var appHeader = {
+  template: '<div>Header</div>',
+  methods: {
+    addNum() {
+
+    }
+  }
+}
+
+<template>
+  <div>Header</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    getNum() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
