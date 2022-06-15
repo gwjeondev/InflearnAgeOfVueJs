076d555b2de5f73a1a48de3a80d56762ca0688e7
gwjeondev <gw4609@gmail.com>
2022-06-15 13:18:13 +0900

    싱글 파일 컴포넌트 - 싱글 파일 컴포넌트에 배운 내용 적용하여 개발 시작하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- template root는 무조건 하나만 있어야함. -->
+  <div>
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      str: 'hi'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
