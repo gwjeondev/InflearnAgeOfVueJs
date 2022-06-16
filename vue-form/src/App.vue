e044fcc3cf59a83c56def4ee3fea4207a8da2b95
gwjeondev <gw4609@gmail.com>
2022-06-16 13:29:44 +0900

    최종 프로젝트 - 사용자 입력 폼 만들기 - axios를 이용한 데이터 전송 및 form 구현

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <form v-on:submit="submitForm">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text" v-model="username"/>
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password" v-model="password"/>
+      <input id="password" type="password" v-model="password" />
     </div>
     <button type="submit">login</button>
   </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       username: '',
       password: ''
-    }
+    };
   },
   methods: {
-    submitForm(e) {
-      e.preventDefault();
+    submitForm() {
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      //e.preventDefault(); form element에 디렉티브 속성에 .prevent를 추가해주면 동일한 기능을 보장한다.
       console.log(this.username, this.password);
+      axios
+        .post(url, {
+          username: this.username,
+          password: this.password
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }
 };
