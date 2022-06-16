53c59ffe27dc7b156822430a5b22b417dad8d860
gwjeondev <gw4609@gmail.com>
2022-06-16 11:44:31 +0900

    최종 프로젝트 - 사용자 입력 폼 만들기 - v-model 속성과 submit 이벤트 처리

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <form action="">
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username"/>
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password" />
+      <input id="password" type="password" v-model="password"/>
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      console.log(this.username, this.password);
+    }
+  }
+};
 </script>
 
 <style>
